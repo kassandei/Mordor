3989d8ccb24b4a5671fa698fcce0225c139e3f00
kassa <kassandei839@gmail.com>
2025-11-07 17:27:45 +0100

    aggiunti dei membri alla struct Player

--- include/types.h
+++ include/types.h
@@ -3,13 +3,19 @@
 
 #define NAMESIZE 20
 #define QUESTS 3
+#define MAX_HP 20
+#include <stdbool.h>
 
 typedef struct {
     char name[NAMESIZE];
     int hp;
-    int money;
-    int items;
-    int missionComplete[QUESTS];
+    int coins;
+    int potions;
+    bool hasDmgBuff;
+    bool hasArmor;
+    bool hasCastleKey;
+    bool hasHeroSword;
+    bool missionComplete[QUESTS];
 } Player;
 
 
